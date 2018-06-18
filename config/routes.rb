01243a845927cdeb6ea9eb60d5666a6062d7b8f6
takeshi.ryota <takeshi.ryota.prog@gmail.com>
2018-06-18 15:13:00 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   
   
   resources :users,only:[:show,:new,:create]
+  
+  resources :items,only:[:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
