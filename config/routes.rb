324e4ddf9d07fa20bde3192b220d50997d0fb246
takeshi.ryota <takeshi.ryota.prog@gmail.com>
2018-06-18 16:06:01 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   
   resources :users,only:[:show,:new,:create]
   
-  resources :items,only:[:new]
+  resources :items,only:[:new,:show]
+  resources :ownerships,only:[:create,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
