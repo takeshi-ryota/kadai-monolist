04dc297996ebd98c6e68689e4675b8bbf8d0a906
takeshi.ryota <takeshi.ryota.prog@gmail.com>
2018-06-19 05:29:14 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -9,15 +9,26 @@ class OwnershipsController < ApplicationController
       @item.save
     end
     
+    if params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品をhaveしました'
+    end
+    
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品をwantしました'
     end
+    
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @item = Item.find(params[:item_id])
+    
+    if params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品のhaveを解除'
+    end
     
     if params[:type] == 'Want'
       current_user.unwant(@item)
